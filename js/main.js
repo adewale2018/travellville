b81b1a0f00795196e0e36e56446a11208c696450
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-05-04 15:03:23 +0100

    added slider section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
   $(".button-collapse").sideNav();
+
+  $(".slider").slider({
+    indicators: false,
+    transition: 500,
+    interval: 6000,
+    height: 500
+  });
 });
